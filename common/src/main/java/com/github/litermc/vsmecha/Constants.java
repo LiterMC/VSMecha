99d62798c175c773fddd26962c91b56de85bc5fa
zyxkad <zyxkad@gmail.com>
2026-04-19 20:09:46 -0600

    bump version to v0.0.9

--- common/src/main/java/com/github/litermc/vsmecha/Constants.java
+++ common/src/main/java/com/github/litermc/vsmecha/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public final class Constants {
 	public static final String MOD_ID = "vsmecha";
 	public static final String MOD_NAME = "VSMecha";
-	public static final String MOD_VERSION = "0.0.8";
+	public static final String MOD_VERSION = "0.0.9";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
 	private Constants() {}
